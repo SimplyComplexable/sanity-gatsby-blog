3793ff2e1a9f423a47815dfc988b70dcdfbbbce0
Zack Sunderland <simplycomplexable@gmail.com>
2020-03-19 12:25:50 -0600

    Remove limit on the archive

--- web/src/pages/archive.js
+++ web/src/pages/archive.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import {graphql} from 'gatsby'
-import {mapEdgesToNodes} from '../lib/helpers'
-import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
-import Container from '../components/container'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
+import React from "react";
+import { graphql } from "gatsby";
+import { mapEdgesToNodes } from "../lib/helpers";
+import BlogPostPreviewGrid from "../components/blog-post-preview-grid";
+import Container from "../components/container";
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
 
-import {responsiveTitle1} from '../components/typography.module.css'
+import { responsiveTitle1 } from "../components/typography.module.css";
 
 export const query = graphql`
   query ArchivePageQuery {
-    posts: allSanityPost(limit: 12, sort: {fields: [publishedAt], order: DESC}) {
+    posts: allSanityPost(sort: { fields: [publishedAt], order: DESC }) {
       edges {
         node {
           id
@@ -28,22 +28,22 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
 const ArchivePage = props => {
-  const {data, errors} = props
+  const { data, errors } = props;
 
-  const postNodes = data && data.posts && mapEdgesToNodes(data.posts)
+  const postNodes = data && data.posts && mapEdgesToNodes(data.posts);
 
   return (
     <Layout>
-      <SEO title='Archive' />
+      <SEO title="Archive" />
       <Container>
         <h1 className={responsiveTitle1}>Archive</h1>
         {postNodes && postNodes.length > 0 && <BlogPostPreviewGrid nodes={postNodes} />}
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default ArchivePage
+export default ArchivePage;
